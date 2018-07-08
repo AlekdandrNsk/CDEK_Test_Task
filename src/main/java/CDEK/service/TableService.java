78e3858dcf389659ff475839827cd1f45b071913
AlekdandrNsk <aleksparshukov@yandex.ru>
2018-07-08 23:32:05 +0700

    cash

--- src/main/java/CDEK/service/TableService.java
+++ src/main/java/CDEK/service/TableService.java
@@ -5,6 +5,8 @@ import CDEK.repository.JdbcRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,11 +18,13 @@ public class TableService {
     @Autowired
     JdbcRepository repository;
 
+    @CacheEvict(value = "entities", allEntries = true)
     public Entity save(String name){
         log.info("save entity with name {}", name);
         return repository.save(name);
     }
 
+    @Cacheable("entities")
     public List<Entity> getAll(){
         log.info("get all entities");
         return repository.getAll();
