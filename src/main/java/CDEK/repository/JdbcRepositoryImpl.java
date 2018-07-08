e7ac802344c8f06f0aa43455c636dce42fca6ddf
AlekdandrNsk <aleksparshukov@yandex.ru>
2018-07-09 03:53:02 +0700

    intrfcs

---
+++ src/main/java/CDEK/repository/JdbcRepositoryImpl.java
@@ -0,0 +1,54 @@
+package CDEK.repository;
+
+import CDEK.model.Entity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+@Repository
+@Transactional(readOnly = true)
+public class JdbcRepositoryImpl implements JdbcRepository {
+
+    private static final BeanPropertyRowMapper<Entity> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Entity.class);
+
+    private final JdbcTemplate jdbcTemplate;
+
+    private final SimpleJdbcInsert insertEntity;
+
+    @Autowired
+    public JdbcRepositoryImpl(DataSource dataSource, JdbcTemplate jdbcTemplate) {
+        this.insertEntity = new SimpleJdbcInsert(dataSource)
+                .withTableName("table_CDEK")
+                .usingGeneratedKeyColumns("id");
+
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    @Transactional
+    public Entity save(String name) {
+        MapSqlParameterSource map = new MapSqlParameterSource()
+                .addValue("name", name);
+
+        Number id = insertEntity.executeAndReturnKey(map);
+
+        return new Entity(id.intValue(), name);
+    }
+
+    @Override
+    public List<Entity> getAll() {
+        return jdbcTemplate.query("SELECT * FROM table_CDEK", ROW_MAPPER);
+    }
+
+    @Override
+    public List<Entity> getByName(String name) {
+        return jdbcTemplate.query("SELECT * FROM table_CDEK WHERE name = ?", ROW_MAPPER, name);
+    }
+}
---
+++ src/main/java/CDEK/service/TableServiceImpl.java
@@ -0,0 +1,40 @@
+package CDEK.service;
+
+import CDEK.model.Entity;
+import CDEK.repository.JdbcRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TableServiceImpl implements TableService {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    JdbcRepository repository;
+
+    @Override
+    @CacheEvict(value = "entities", allEntries = true)
+    public Entity save(String name) {
+        log.info("save entity with name {}", name);
+        return repository.save(name);
+    }
+
+    @Override
+    @Cacheable("entities")
+    public List<Entity> getAll() {
+        log.info("get all entities");
+        return repository.getAll();
+    }
+
+    @Override
+    public List<Entity> getByName(String name) {
+        log.info("get entities with name {}", name);
+        return repository.getByName(name);
+    }
+}
